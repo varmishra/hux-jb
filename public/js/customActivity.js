fa95349dcb45cc153e65402a08937f79bbcf34a0
varmishra <55395082+varmishra@users.noreply.github.com>
2020-01-27 17:56:05 +0530

    Update

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -15,7 +15,7 @@ define([
     connection.on('requestedEndpoints', onGetEndpoints);
 
     connection.on('clickedNext', save);
-   
+
     function onRender() {
         // JB will respond the first time 'ready' is called with 'initActivity'
         connection.trigger('ready');
@@ -30,7 +30,7 @@ define([
         if (data) {
             payload = data;
         }
-        
+
         var hasInArguments = Boolean(
             payload['arguments'] &&
             payload['arguments'].execute &&
@@ -44,8 +44,8 @@ define([
 
         $.each(inArguments, function (index, inArgument) {
             $.each(inArgument, function (key, val) {
-                
-              
+
+
             });
         });
 
@@ -71,9 +71,10 @@ define([
 
         payload['arguments'].execute.inArguments = [{
             "tokens": authTokens,
+            //UID
             "emailAddress": "{{Contact.Attribute.PostcardJourney.EmailAddress}}"
         }];
-        
+
         payload['metaData'].isConfigured = true;
 
         console.log(payload);
