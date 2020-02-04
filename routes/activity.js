b6ec0300bc061a4267ad4584d5d1485cc9468696
varmishra <55395082+varmishra@users.noreply.github.com>
2020-02-04 20:45:40 +0530

    Updated Config.json

--- routes/activity.js
+++ routes/activity.js
@@ -129,5 +129,7 @@ exports.validate = function (req, res) {
   // Data from the req and put it in an array accessible to the main app.
   //console.log( req.body );
   logData(req);
-  res.send(200, "Validate");
+  res.send(200).json({
+    "success": true
+  });
 };
